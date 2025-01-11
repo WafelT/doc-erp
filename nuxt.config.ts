962f8858e9a529f13e0404314f55afd45063793e
Dmitrii Turcanu <dumitru_turcanu@alsodev.md>
2025-01-11 18:17:09 +0200

    [update] add fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,26 +4,39 @@ import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
     compatibilityDate: '2024-11-01',
     devtools: { enabled: true },
-    // app: {
-    //     head: {
-    //         link: [
-    //             {
-    //                 rel: 'stylesheet',
-    //                 href: 'https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css',
-    //                 integrity: 'sha512-NmLkDIU1C/C88wi324HBc+S2kLhi08PN5GDeUVVVC/BVt/9Izdsc9SVeVfA1UZbY3sHUlDSyRXhCzHfr6hmPPw==',
-    //                 crossorigin: 'anonymous',
-    //                 referrerpolicy: 'no-referrer'
-    //             },
-    //             {
-    //                 rel: 'stylesheet',
-    //                 href: 'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
-    //                 integrity: 'sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==',
-    //                 crossorigin: 'anonymous',
-    //                 referrerpolicy: 'no-referrer'
-    //             }
-    //         ]
-    //     }
-    // },
+    app: {
+        head: {
+            link: [
+                {
+                    rel: 'stylesheet',
+                    href: 'https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css',
+                    integrity: 'sha512-NmLkDIU1C/C88wi324HBc+S2kLhi08PN5GDeUVVVC/BVt/9Izdsc9SVeVfA1UZbY3sHUlDSyRXhCzHfr6hmPPw==',
+                    crossorigin: 'anonymous',
+                    referrerpolicy: 'no-referrer'
+                },
+                {
+                    rel: 'stylesheet',
+                    href: 'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
+                    integrity: 'sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==',
+                    crossorigin: 'anonymous',
+                    referrerpolicy: 'no-referrer'
+                },
+                {
+                    rel: 'preconnect',
+                    href: 'https://fonts.googleapis.com'
+                },
+                {
+                    rel: 'preconnect',
+                    href: 'https://fonts.gstatic.com',
+                    crossorigin: 'anonymous'
+                },
+                {
+                    rel: 'stylesheet',
+                    href: 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap&family=Source+Code+Pro:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'
+                }
+            ]
+        }
+    },
 
     modules: [
         '@nuxt/image',
